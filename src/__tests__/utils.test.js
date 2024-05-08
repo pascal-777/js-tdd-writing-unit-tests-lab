05644db999ef79dc7ada815602a8f296da0e8c9d
pascal <pascal.nicintije@student.moringaschool.com>
2024-05-08 11:00:46 +0300

    "completes lab"

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,18 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true when the word is the same forward and backward", () => {
+        const word = "racecar";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(true);
+    });
+    it("returns false when the word is not the same forward and backward", () => {
+        const word = "car";
+
+        const result = isPalindrome(word);
+
+        expect(result).toBe(false);
+    });
+})
